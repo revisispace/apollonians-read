9dd6de9679f9469a675ef44e9123a5a97f04fd70
revisispace <nzachra19@gmail.com>
2026-08-06 09:29:45 +0700

    Load chapter management styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./responsive.css";
 import "./mobile-player.css";
 import "./header-tools.css";
 import "./storage-management.css";
+import "./chapter-management.css";
 import { AuthProvider } from "./lib/auth";
 import { ServiceWorkerRegistration } from "./components/ServiceWorkerRegistration";
 
